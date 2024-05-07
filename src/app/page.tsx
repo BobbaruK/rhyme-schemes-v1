790ac7f40b6413bc844b2dc909b72f7b44e7f1e7
Bobbaru <bobbaru_krueger@yahoo.com>
2024-05-07 22:23:45 +0300

    fetch albums and songs from contentful and render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { IAlbum } from "@/types/Album";
 import { IAsset } from "@/types/Asset";
-import { createClient } from "contentful";
+import { ISong } from "@/types/Song";
+import { createClient, EntrySkeletonType } from "contentful";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,37 +12,57 @@ export default async function Home() {
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as unknown as string,
   });
 
-  const res = await client.getEntries({ content_type: "album" });
+  const albums = await client.getEntries({ content_type: "album" });
+
+  // Songs
+  const songsResponse = await client.getEntries<
+    EntrySkeletonType<ISong>,
+    string
+  >({ content_type: "song" });
+  const songs = songsResponse.items;
+
+  const getFirstTrackId = (albumId: string) => {
+    const albumSongs = songs.filter((song) => song.fields.albumId === albumId);
+
+    const firstTrack = albumSongs.find((song) => {
+      const trackNo = parseInt(song.fields.trackNo);
+
+      return trackNo === 1;
+    });
+
+    return firstTrack?.fields.id;
+  };
 
   return (
     <>
       <div className="container py-8 flex gap-4 flex-col">
-        {res.items.map((album) => {
+        {albums.items.map((album) => {
           const actualAlbum = album.fields as unknown as IAlbum;
-          const albumAsset = res.includes?.Asset as unknown as IAsset[];
+          const albumAsset = albums.includes?.Asset as unknown as IAsset[];
 
           const albumCover = albumAsset.find(
-            (asset) => asset.sys.id === actualAlbum.albumArtwork.sys.id
+            (asset) => asset.sys.id === actualAlbum.artwork.sys.id
           );
+
+          const firstTrackId = getFirstTrackId(actualAlbum.id);
 
           return (
             <Card
-              key={actualAlbum.albumSlug}
+              key={actualAlbum.slug}
               className="flex justify-start items-center overflow-hidden flex-col text-center sm:flex-row sm:text-start">
-              <Link href={actualAlbum.albumSlug}>
+              <Link href={`/${actualAlbum.slug}/${firstTrackId}`}>
                 <Image
                   src={"https:" + albumCover?.fields.file.url}
-                  alt={actualAlbum.albumName}
+                  alt={actualAlbum.name}
                   width={albumCover?.fields.file.details.image.width}
                   height={albumCover?.fields.file.details.image.height}
                   className="max-w-full h-auto sm:max-w-[200px] sm:max-h-[150px] object-cover"
-                />{" "}
+                />
               </Link>
               <CardHeader>
                 <CardTitle>
-                  <Link href={actualAlbum.albumSlug}>
-                    {actualAlbum.albumName}{" "}
-                    <small>({actualAlbum.albumYear})</small>
+                  <Link href={`/${actualAlbum.slug}/${firstTrackId}`}>
+                    {actualAlbum.name} <small>({actualAlbum.year})</small>
                   </Link>
                 </CardTitle>
               </CardHeader>
